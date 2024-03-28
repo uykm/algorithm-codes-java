03e0c4bb3f303168cfdaafa4512abbc640b17a35
Minkyu Shin <98581610+uykm@users.noreply.github.com>
2024-03-28 23:43:57 +0900

    [level 1] Title: 자연수 뒤집어 배열로 만들기, Time: 0.04 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.java
+++ 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.java
@@ -1,6 +1,7 @@
 class Solution {
     public int[] solution(long n) {
-        String str = n + "";
+        String str = Long.toString(n); 
+        // 문자열 연산은 객체를 생성하기 때문에, 메모리 사용 측면에서 비효율적 !
         int[] answer = new int[str.length()];
         
         for(int i = 0; i < str.length(); i++) {
