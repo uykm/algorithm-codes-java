016c224aa2ae6c5d5b07a9be846be7a4810a4cb9
Minkyu Shin <98581610+uykm@users.noreply.github.com>
2024-04-04 02:44:03 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 33.28 ms, Memory: 117 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -14,8 +14,10 @@ public class Solution {
             prevNum = curNum;
         }
         
+        // 메모리는 많이 잡아먹긴 하는데, 편의상 사용
         return list.stream()
-            .mapToInt(i -> i)
+            .mapToInt(i -> i) // 'mapToInt'는 객체 스트림을을 int 스트림으로 변환해주고, 람다표현식 'i -> i'는 아무 작업 X
+            // 'Integer::intValue)': Object 타입을 int 타입으로 변환
             .toArray();
     }
 }
