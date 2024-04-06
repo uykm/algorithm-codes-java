86b5371171a6b3e9cc2ef0ec97771097893f15de
Minkyu Shin <98581610+uykm@users.noreply.github.com>
2024-04-07 02:44:07 +0900

    [level 1] Title: 예산, Time: 0.33 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.java
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -1,6 +1,17 @@
+import java.util.*;
+
 class Solution {
     public int solution(int[] d, int budget) {
         int answer = 0;
+        Arrays.sort(d);
+        
+        for (int request : d) {
+            budget -= request;
+            if (budget < 0) {
+                break;
+            }
+            answer++;
+        }
         return answer;
     }
 }
