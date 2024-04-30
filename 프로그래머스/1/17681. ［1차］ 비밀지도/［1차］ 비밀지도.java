8be8b22ceccbe93eae7f69af45d0e7c8212ecb60
Minkyu Shin <98581610+uykm@users.noreply.github.com>
2024-04-30 17:37:58 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.06 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -8,7 +8,7 @@ class Solution {
         
         for(int i = 0; i < n; ++i) {
             int tmp = arr1[i];
-            boolean flag;
+            // arr1을 2진수로 변환
             for(int j = 0; j < n; ++j) {
                 sb.append(tmp % 2);
                 tmp /= 2;
@@ -17,6 +17,7 @@ class Solution {
             
             sb = new StringBuilder();
             tmp = arr2[i];
+            // arr2를 2진수로 변환
             for(int j = 0; j < n; ++j) {
                 sb.append(tmp % 2);
                 tmp /= 2;
@@ -29,6 +30,7 @@ class Solution {
             for(int j = 0 ; j < n; ++j) {
                 int binaryBit1 = row1[i].charAt(j) - '0';
                 int binaryBit2 = row2[i].charAt(j) - '0';
+                // 비트 연산자는 피연산자로 10진수를 넣어도 정상적으로 작동하더라
                 sb.append((binaryBit1 | binaryBit2) == 1 ? "#" : " ");
             }
             result[i] = sb.toString();
@@ -37,3 +39,32 @@ class Solution {
         return result;
     }
 }
+
+/*
+
+class Solution { 
+    public String[] solution(int n, int[] arr1, int[] arr2) {
+        String[] answer = new String[n];
+        
+        for(int i = 0; i < arr1.length; ++i)
+            answer[i] = fomat(n, Integer.toBinaryString(arr1[i]|arr2[i]));
+        
+        return answer;
+    }
+    
+    // n칸 맞추기 + " #  #" 형태로 포맷팅하기 
+    public String fomat(int n, String binary) {
+        StringBuilder result = new StringBuilder();
+        int l = binary.length();
+        
+        for(int i = 0; i < n - l; ++i) result.append(" ");
+        for(int i = 0; i < l; ++i) {
+            if(binary.charAt(i) == '1') result.append("#");
+            else result.append(" ");
+        }
+        
+        return result.toString();
+    }
+}
+
+*/
