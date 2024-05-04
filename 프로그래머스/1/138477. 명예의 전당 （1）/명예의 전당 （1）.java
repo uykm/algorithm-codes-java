2adc3a723ebc66f15fbe867cdb6b17d306bd196f
Minkyu Shin <98581610+uykm@users.noreply.github.com>
2024-05-04 21:05:54 +0900

    [level 1] Title: 명예의 전당 (1), Time: 0.27 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -5,11 +5,21 @@ class Solution {
         List<Integer> list = new ArrayList<>();
         int[] answer = new int[score.length];
         
+        /*
+        Arrays.sort() vs Collections.sort()
+        [ Worst case에서의 시간 복잡도 ]
+        - Arrays.sort(): O(n^2)
+        - Collections.sort(): O(n * logn)
+        => 평균적으로 Collections.sort()가 효율적 
+        (`Quick sort` vs `Tim sort(합병 + 삽입)`)
+        */
+        
         
         for (int i = 0; i < score.length; ++i) {
             
             if (i < k) list.add(score[i]);
             else {
+                // k일 이후
                 if (score[i] > list.get(list.size() - 1))
                     list.set(list.size() - 1, score[i]);
             }
