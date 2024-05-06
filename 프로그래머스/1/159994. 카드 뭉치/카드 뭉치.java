61a0dde6ff279d68aac9ecef47522cbf44036212
Minkyu Shin <98581610+uykm@users.noreply.github.com>
2024-05-07 00:25:03 +0900

    [level 1] Title: 카드 뭉치, Time: 0.02 ms, Memory: 71.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String solution(String[] cards1, String[] cards2, String[] goal) {
+        boolean flag = true;
+        int index1 = 0;
+        int index2 = 0;
+        
+        // cards1 순서대로 나오는지 체크
+        for (int i = 0; i < goal.length; ++i) {
+            if (index1 < cards1.length && cards1[index1].equals(goal[i])) {
+                index1++;
+                continue;
+            }
+            
+            if (index2 < cards2.length && cards2[index2].equals(goal[i])) {
+                index2++;
+                continue;
+            }
+            
+            flag = false;
+            break;
+        }
+        
+        return flag ? "Yes" : "No";
+    }
+}
