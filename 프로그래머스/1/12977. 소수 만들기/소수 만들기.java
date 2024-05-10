343792e9c184c4bce296e33d1df5bc3a9615235b
Minkyu Shin <98581610+uykm@users.noreply.github.com>
2024-05-10 20:17:20 +0900

    [level 1] Title: 소수 만들기, Time: 0.01 ms, Memory: 74.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+        int sum = 0;
+
+        for (int i = 0; i < nums.length; ++i) {
+            for (int j = i + 1; j < nums.length; ++j) {
+                for (int k = j + 1; k < nums.length; ++k) {
+                    sum = nums[i] + nums[j] + nums[k];
+                    if (isPrime(sum)) answer++;
+                }
+            }
+        }
+
+        return answer;
+    }
+    
+    public boolean isPrime(int sum) {
+        for (int i = 2; i < sum/2; ++i) {
+            if (sum % i == 0) {
+                return false;
+            }            
+        }
+        return true;
+    }
+}
