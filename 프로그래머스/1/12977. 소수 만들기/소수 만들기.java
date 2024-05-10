1cae635a294e6acb291e48810c957dbe1f1964d3
Minkyu Shin <98581610+uykm@users.noreply.github.com>
2024-05-10 20:17:54 +0900

    [level 1] Title: 소수 만들기, Time: 0.02 ms, Memory: 72.3 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
@@ -3,9 +3,9 @@ import java.util.*;
 class Solution {
     public int solution(int[] nums) {
         int answer = 0;
-        int sum = 0;
 
         for (int i = 0; i < nums.length; ++i) {
+            int sum = 0;
             for (int j = i + 1; j < nums.length; ++j) {
                 for (int k = j + 1; k < nums.length; ++k) {
                     sum = nums[i] + nums[j] + nums[k];
