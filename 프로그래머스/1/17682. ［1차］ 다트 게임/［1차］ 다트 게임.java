0b6830426a7b42958ac844c6d4d77b60b8aa1778
Minkyu Shin <98581610+uykm@users.noreply.github.com>
2024-05-16 04:22:23 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.08 ms, Memory: 76.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.java
@@ -0,0 +1,55 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String dartResult) {
+        int totalScore = 0;
+        int[] scores = new int[3];  // 세 번의 점수를 저장할 배열
+        int index = 0;              // 현재 점수 인덱스
+        
+        int length = dartResult.length();
+        int i = 0;
+        
+        while (i < length) {
+            // 점수 계산
+            StringBuilder scoreBuilder = new StringBuilder();
+            while (i < length && Character.isDigit(dartResult.charAt(i))) { // 점수 추출
+                scoreBuilder.append(dartResult.charAt(i));
+                i++;
+            }
+            int score = Integer.parseInt(scoreBuilder.toString());
+            
+            // 보너스 계산
+            char bonus = dartResult.charAt(i);
+            i++;
+            if (bonus == 'S') {
+                score = (int) Math.pow(score, 1);
+            } else if (bonus == 'D') {
+                score = (int) Math.pow(score, 2);
+            } else if (bonus == 'T') {
+                score = (int) Math.pow(score, 3);
+            }
+            
+            // 옵션 계산
+            if (i < length && (dartResult.charAt(i) == '*' || dartResult.charAt(i) == '#')) {
+                char option = dartResult.charAt(i);
+                if (option == '*') {
+                    score *= 2;
+                    if (index > 0) {
+                        scores[index - 1] *= 2;
+                    }
+                } else if (option == '#') {
+                    score *= -1;
+                }
+                i++;
+            }
+            
+            scores[index++] = score;
+        }
+        
+        for (int s : scores) {
+            totalScore += s;
+        }
+        
+        return totalScore;
+    }
+}
