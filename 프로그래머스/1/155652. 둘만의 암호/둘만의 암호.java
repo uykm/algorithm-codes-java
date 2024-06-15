daf4ad2180c69397aae8a26caaf741a8c6597cfc
Minkyu Shin <98581610+uykm@users.noreply.github.com>
2024-06-15 23:06:32 +0900

    [level 1] Title: 둘만의 암호, Time: 0.35 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.java
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.java
@@ -1,3 +1,30 @@
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public String solution(String s, String skip, int index) {
+        StringBuilder sb = new StringBuilder();
+        Set<Character> skipSet = new HashSet<>();
+        for (char sk : skip.toCharArray()) {
+            skipSet.add(sk);
+        }
+
+        for (char c : s.toCharArray()) {
+            int i = 0;
+            char pwd = c;
+            while (i < index) {
+                pwd = (pwd == 'z') ? 'a' : (char)(pwd + 1);
+                if (!skipSet.contains(pwd)) { 
+                    i++; 
+                }
+            }
+            sb.append(pwd);
+        }
+        
+        return sb.toString();
+    }
+}
+/*
 class Solution {
     public String solution(String s, String skip, int index) {
         StringBuilder sb = new StringBuilder();
@@ -15,3 +42,4 @@ class Solution {
         return sb.toString();
     }
 }
+*/
