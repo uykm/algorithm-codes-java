304acd8dce26eb4542a11ad1b439e831ad29ab8c
Minkyu Shin <98581610+uykm@users.noreply.github.com>
2024-06-15 23:09:27 +0900

    [level 1] Title: 둘만의 암호, Time: 0.82 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.java
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.java
@@ -1,40 +1,23 @@
-import java.util.HashSet;
-import java.util.Set;
-
-class Solution {
-    public String solution(String s, String skip, int index) {
-        StringBuilder sb = new StringBuilder();
-        Set<Character> skipSet = new HashSet<>();
-        for (char sk : skip.toCharArray()) {
-            skipSet.add(sk);
-        }
-
-        for (char c : s.toCharArray()) {
-            int i = 0;
-            char pwd = c;
-            while (i < index) {
-                pwd = (pwd == 'z') ? 'a' : (char)(pwd + 1);
-                if (!skipSet.contains(pwd)) { 
-                    i++; 
-                }
-            }
-            sb.append(pwd);
-        }
-        
-        return sb.toString();
-    }
-}
-/*
 class Solution {
     public String solution(String s, String skip, int index) {
         StringBuilder sb = new StringBuilder();
         for (char c : s.toCharArray()) {
             int i = 0;
-            char pwd = c;
+            int pwd = c;
             while (i < index) {
                 boolean doSkip = false;
-                pwd = (pwd == 'z') ? pwd = 'a' : (char)(pwd + 1);
-                if (!skip.contains(String.valueOf(pwd))) { i++; }
+                pwd = pwd + 1;
+                if (pwd > 'z') {
+                    pwd = 'a';
+                }
+                for (char sk : skip.toCharArray()) {
+                    if (pwd == sk) {
+                    	doSkip = true;
+                        break;
+                    }
+                }
+                if (doSkip) continue;
+                i++;
             }
             sb.append((char)pwd);
         }
@@ -42,4 +25,3 @@ class Solution {
         return sb.toString();
     }
 }
-*/
