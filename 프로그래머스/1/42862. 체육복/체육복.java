f687980c574695b836e2a315285aea64882da978
Minkyu Shin <98581610+uykm@users.noreply.github.com>
2024-06-16 18:28:19 +0900

    [level 1] Title: 체육복, Time: 0.02 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -4,10 +4,12 @@ class Solution {
         boolean[] lostCheck = new boolean[n];
         boolean[] reserveCheck = new boolean[n];
         
+        // 도난 당한 사람 체크
         for (int lst : lost) {
             lostCheck[lst-1] = true;
         }
         
+        // 여벌 있는 사람 체크
         for (int rsv : reserve) {
             reserveCheck[rsv-1] = true;
         }
@@ -20,18 +22,23 @@ class Solution {
             }
         }
         
+        // 도난 당한 사람들 중 본인에게 여벌이 없고, 빌릴 수 있는 경우 체크
         for (int i = 0; i < n; ++i) {
             if (lostCheck[i]) {
+                // 앞 사람에게 여벌이 있는 경우
                 if ((i > 0) && reserveCheck[i-1]) {
                     lostCheck[i] = false;
                     reserveCheck[i-1] = false;
-                } else if ((i < n-1) && reserveCheck[i+1]) {
+                } 
+                // 뒷 사람에게 여벌이 있는 경우
+                else if ((i < n-1) && reserveCheck[i+1]) {
                     lostCheck[i] = false;
                     reserveCheck[i+1] = false;
                 }
             }
         }
         
+        // 빌리지 못한 경우 체크
         for (boolean lc : lostCheck) {
             if (lc) max--;
         }
