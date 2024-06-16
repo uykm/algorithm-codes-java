997feee5c54790e78ae8553f1ec8c9f0c74e8056
Minkyu Shin <98581610+uykm@users.noreply.github.com>
2024-06-16 19:04:34 +0900

    [level 1] Title: 체육복, Time: 0.03 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -1,3 +1,4 @@
+/*
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
         int max = n;
@@ -43,6 +44,42 @@ class Solution {
             if (lc) max--;
         }
         
+        return max;
+    }
+}
+*/
+
+
+class Solution {
+    public int solution(int n, int[] lost, int[] reserve) {
+        int[] suitsPerStudent = new int[n];
+        int max = n;
+
+        // 도난 당한 학생들의 체육복 개수 업데이트
+        for (int l : lost) 
+            suitsPerStudent[l-1]--;
+        
+        // 여분이 있는 학생들의 체육복 개수 업데이트
+        for (int r : reserve) 
+            suitsPerStudent[r-1]++;
+
+        for (int i = 0; i < n; i++) {
+            // 도난 당하고 여분도 없는 학생의 경우
+            if (suitsPerStudent[i] == -1) {
+                // 앞 사람에게 빌릴 수 있는 경우
+                if ((i-1>=0) && (suitsPerStudent[i-1]==1)) {
+                    suitsPerStudent[i]++;
+                    suitsPerStudent[i-1]--;
+                
+                } 
+                // 뒷 사람에게 빌릴 수 있는 경우
+                else if((i+1<n) && (suitsPerStudent[i+1] == 1)) {
+                    suitsPerStudent[i]++;
+                    suitsPerStudent[i+1]--;
+                } else 
+                    max--;
+            }
+        }
         return max;
     }
 }
