b12bc43d83a5068105fedb5b759d0a517548d58f
Minkyu Shin <98581610+uykm@users.noreply.github.com>
2024-06-18 20:05:53 +0900

    [level 1] Title: 햄버거 만들기, Time: 0.16 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.java
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.java
@@ -1,36 +1,27 @@
-// class Solution {
-//     public int solution(int[] ingredient) {
-//         int hamburgers = 0;
-//         StringBuilder sb = new StringBuilder();
-        
-//         for (int i : ingredient) {
-//             sb.append(i);
-//             // 만약 현재 마지막 4글자가 "1231"이라면
-//             if (sb.length() >= 4 && sb.substring(sb.length() - 4).equals("1231")) {
-//                 sb.delete(sb.length() - 4, sb.length());
-//                 hamburgers++;
-//             }
-//         }
-        
-//         return hamburgers;
-//     }
-// }
+import java.util.Stack;
 
 class Solution {
     public int solution(int[] ingredient) {
-        int[] stack = new int[ingredient.length];
-        int sp = 0;
-        int answer = 0;
+        Stack<Integer> stack = new Stack<>();
+        int hamburgers = 0;
+
         for (int i : ingredient) {
-            stack[sp++] = i;
-            if (sp >= 4 && stack[sp - 1] == 1
-                && stack[sp - 2] == 3
-                && stack[sp - 3] == 2
-                && stack[sp - 4] == 1) {
-                sp -= 4;
-                answer++;
+            stack.push(i);
+            if (stack.size() >= 4) {
+                // 마지막 4개의 요소가 "1, 2, 3, 1"인지 확인
+                if (stack.get(stack.size() - 4) == 1 &&
+                    stack.get(stack.size() - 3) == 2 &&
+                    stack.get(stack.size() - 2) == 3 &&
+                    stack.get(stack.size() - 1) == 1) {
+                    // "1231" 패턴이 발견되면 스택에서 제거
+                    stack.pop();
+                    stack.pop();
+                    stack.pop();
+                    stack.pop();
+                    hamburgers++;
+                }
             }
         }
-        return answer;
+        return hamburgers;
     }
 }
