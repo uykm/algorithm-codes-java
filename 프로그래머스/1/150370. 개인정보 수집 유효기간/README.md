62a573ecd84b5958623b932af1a300607202b104
Minkyu Shin <98581610+uykm@users.noreply.github.com>
2024-06-25 03:54:08 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 1.12 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/README.md
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/README.md
@@ -1,10 +1,10 @@
 # [level 1] 개인정보 수집 유효기간 - 150370 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/150370#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/150370) 
 
 ### 성능 요약
 
-메모리: 74.9 MB, 시간: 1.20 ms
+메모리: 73 MB, 시간: 1.12 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 25일 03:35:01
+2024년 06월 25일 03:54:05
 
 ### 문제 설명
 
--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -9,50 +9,18 @@ class Solution {
             hsmp.put(term[0], Integer.valueOf(term[1]));
         }
         
-        String[] td = today.split("\\.");
-        int[] tdInt = new int[3];
-        for (int i = 0; i < 3; ++i) {
-            tdInt[i] = Integer.valueOf(td[i]);
-        }
+        int tdDate = getDate(today);
         
         // 파기해야 할 개인정보 리스트
         List<Integer> list = new ArrayList<>();
         for (int i = 0; i < privacies.length; ++i) {
-            boolean isValid = false;
             // privacy[0] : 유효기간, privacy[1] : 약관 종류
             String[] privacy = privacies[i].split(" ");
             
-            // date[0] : 연도, date[1] : 월, date[2] : 일
-            String[] date = privacy[0].split("\\.");
-            int[] dateInt = new int[3];
-            for (int j = 0; j < 3; ++j) {
-                dateInt[j] = Integer.valueOf(date[j]);
-            }
+            // 일수로 변환하고 약관에 해당하는 기간을 더한다.
+            int tmpDate = getDate(privacy[0]) + hsmp.get(privacy[1]) * 28;
             
-            int month = dateInt[1];
-            int term = hsmp.get(privacy[1]);
-            if ((month + term) > 12) {
-                if ((month + term) % 12 == 0) {
-                    dateInt[0] += (month + term) / 12 - 1;
-                    dateInt[1] = 12;
-                } else {
-                    dateInt[0] += (month + term) / 12;
-                    dateInt[1] = (month + term) % 12;
-                }
-            } else {
-                dateInt[1] = month + term;
-            }
-            
-            for (int j = 0; j < 3; ++j) {
-                if (dateInt[j] < tdInt[j]) { // 유효 기간이 끝난 경우
-                    break;
-                } else if (dateInt[j] > tdInt[j]) { // 유효 기간이 남아있는 경우
-                    isValid = true;
-                    break;
-                }
-            }
-            
-            if (!isValid) {
+            if (tdDate >= tmpDate) {
                 list.add(i+1);
             }
         }
@@ -69,4 +37,85 @@ class Solution {
         
         return answer;
     }
+    
+    private int getDate(String today) {
+        String[] date = today.split("\\.");
+        int year = Integer.parseInt(date[0]);
+        int month = Integer.parseInt(date[1]);
+        int day = Integer.parseInt(date[2]);
+        return (year * 12 * 28) + (month * 28) + day;
+    }
 }
+
+// import java.util.*;
+
+// class Solution {
+//     public int[] solution(String today, String[] terms, String[] privacies) {
+        
+//         HashMap<String, Integer> hsmp = new HashMap<>();
+//         for (String t : terms) {
+//             String[] term = t.split(" ");
+//             hsmp.put(term[0], Integer.valueOf(term[1]));
+//         }
+        
+//         String[] td = today.split("\\.");
+//         int[] tdInt = new int[3];
+//         for (int i = 0; i < 3; ++i) {
+//             tdInt[i] = Integer.valueOf(td[i]);
+//         }
+        
+//         // 파기해야 할 개인정보 리스트
+//         List<Integer> list = new ArrayList<>();
+//         for (int i = 0; i < privacies.length; ++i) {
+//             boolean isValid = false;
+//             // privacy[0] : 유효기간, privacy[1] : 약관 종류
+//             String[] privacy = privacies[i].split(" ");
+            
+//             // date[0] : 연도, date[1] : 월, date[2] : 일
+//             String[] date = privacy[0].split("\\.");
+//             int[] dateInt = new int[3];
+//             for (int j = 0; j < 3; ++j) {
+//                 dateInt[j] = Integer.valueOf(date[j]);
+//             }
+            
+//             int month = dateInt[1];
+//             int term = hsmp.get(privacy[1]);
+//             if ((month + term) > 12) {
+//                 if ((month + term) % 12 == 0) {
+//                     dateInt[0] += (month + term) / 12 - 1;
+//                     dateInt[1] = 12;
+//                 } else {
+//                     dateInt[0] += (month + term) / 12;
+//                     dateInt[1] = (month + term) % 12;
+//                 }
+//             } else {
+//                 dateInt[1] = month + term;
+//             }
+            
+//             for (int j = 0; j < 3; ++j) {
+//                 if (dateInt[j] < tdInt[j]) { // 유효 기간이 끝난 경우
+//                     break;
+//                 } else if (dateInt[j] > tdInt[j]) { // 유효 기간이 남아있는 경우
+//                     isValid = true;
+//                     break;
+//                 }
+//             }
+            
+//             if (!isValid) {
+//                 list.add(i+1);
+//             }
+//         }
+        
+//         Collections.sort(list);
+        
+//         // 정수형 배열 생성
+//         int[] answer = new int[list.size()];
+        
+//         // 리스트의 요소를 배열에 복사
+//         for (int i = 0; i < list.size(); i++) {
+//             answer[i] = list.get(i);
+//         }
+        
+//         return answer;
+//     }
+// }
