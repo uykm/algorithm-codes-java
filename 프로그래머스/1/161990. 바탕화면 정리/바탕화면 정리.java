f3708abf248fc5a739da49e1c326630a74dc6a92
Minkyu Shin <98581610+uykm@users.noreply.github.com>
2024-06-25 01:57:28 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.11 ms, Memory: 78.8 MB -BaekjoonHub

--- 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.java
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.java
@@ -1,7 +1,7 @@
 class Solution {
     public int[] solution(String[] wallpaper) {
         int[] answer = new int[4];
-        int startX = 50, startY = 0, endX = 0, endY = 0;
+        int startX = 50, startY = 50, endX = 0, endY = 0;
         int col = wallpaper.length;
         int row = wallpaper[0].length();
         boolean firstCheck = true;
@@ -9,18 +9,14 @@ class Solution {
         for (int i = 0; i < col; ++i) {
             for (int j = 0; j < row; ++j) {
                 if (wallpaper[i].charAt(j) == '#') {
-                    
                     // 드래그 시작점의 가로 좌표
                     startX = Math.min(startX, j);
                     
                     // 드래그 끝점의 가로 좌표
                     endX = Math.max(endX, j);
                     
-                    // 첫 번째 파일의 세로 좌표를 드래그 시작점의 세로 좌표로 시정
-                    if (firstCheck) { 
-                        startY = i;
-                        firstCheck = false;
-                    }
+                    // 드래그 시작점의 세로 좌표
+                    startY = Math.min(startY, i);
                     
                     // 드래그 끝점의 세로 좌표
                     endY = i;
