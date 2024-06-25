987b1419a96aca18b5038d9ca1b460da8566b5fa
Minkyu Shin <98581610+uykm@users.noreply.github.com>
2024-06-25 18:19:59 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 붕대 감기, Time: 0.05 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.java
+++ 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.java
@@ -9,42 +9,35 @@ class Solution {
         int currentTime = 0;
         
         for (int i = 0; i < attacks.length; ++i) {
-            System.out.println("[ 누적 시간 : " + attacks[i][0] + " ]");
-            int nextAttackTime = (i > 0 ? attacks[i][0] - attacks[i-1][0] : attacks[0][0]);
-            int damage = attacks[i][1];
+            int damage = attacks[i][1]; // 다음 공격의 피해량
+            int nextAttackTime = (i > 0 ? attacks[i][0] - attacks[i-1][0] : attacks[0][0]); // 다음 공격까지 힐할 수 있는 시간
+            // 다음 공격을 받을 때까지 "붕대 감기" 시전
             for (int j = 1; j <= nextAttackTime; ++j) {
                 if (j == nextAttackTime) { // 공격 당한 경우
                     healingTime = 0;
                     curHealth -= damage;
-                    System.out.println("=> 공격당함 - " + curHealth + " : " + damage + "만큼 감소");
-                    // 피가 0이 된 경우
-                    if (curHealth <= 0) {
+                    
+                    if (curHealth <= 0) { // 피가 0이 된 경우
                         isDead = true;
-                        System.out.println("=> 사망");
                         break;
                     }
                     continue;
                 }
+                
                 healingTime++; // 붕대 감기 시간 1초 증가
-                System.out.println("붕대 감기 - " + healingTime + "초"); 
-                System.out.print("=> " + curHealth + " -> ");
                 curHealth += healPerSec; // 초당 회복
-                System.out.println(curHealth + " : " + healPerSec + "만큼 회복");
-                // 연속으로 붕대 감기 성공
-                if (healingTime == demandTime) {
+    
+                if (healingTime == demandTime) { // 연속으로 붕대 감기 성공
                     healingTime = 0;
                     curHealth += plusHeal; // 추가 회복
-                    System.out.println("=> 연속 붕대 감기 성공 - " + curHealth + " : " + plusHeal + "만큼 회복");
-                }
-                // 최대 체력을 넘긴 경우
-                if (curHealth > health) {
-                    System.out.print("=> 최대 체력을 넘김 - " + curHealth + " -> ");
-                    curHealth = health;
-                    System.out.println(curHealth);
                 }
                 
+                if (curHealth > health) { // 최대 체력을 넘긴 경우
+                    curHealth = health;
+                }
             }
-            if (isDead) {
+            
+            if (isDead) { // 피가 0이 됐는지 체크
                 curHealth = -1;
                 break;
             }
