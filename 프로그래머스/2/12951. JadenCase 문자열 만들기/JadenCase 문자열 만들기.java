10484aedec44177f0b3edab4a05894cd3d0814cc
Minkyu Shin <98581610+uykm@users.noreply.github.com>
2024-06-29 14:34:11 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.15 ms, Memory: 72.6 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -1,32 +1,14 @@
 class Solution {
-    public String solution(String s) {
-        boolean isFirstCh = true;
-        
+  public String solution(String s) {
         StringBuilder sb = new StringBuilder();
-        int head = 0;
-        for (char c : s.toCharArray()) {
-            if (isFirstCh) { // 띄어쓰기 직후 문자
-                if (c == ' ') {
-                    sb.append(" ");
-                    isFirstCh = true;
-                    continue; // 또 띄어쓰기가 나온 경우
-                } else if ('a' <= c && c <= 'z') { // 첫 문자가 소문자 알파벳인 경우
-                    sb.append((char)(c - 32));
-                } else { // 첫 문자이고 대문자인 경우, 숫자이거나 알파벳인 경우
-                    sb.append(c); 
-                }
-            } else {
-                if ('A' <= c && c <= 'Z') {
-                    sb.append((char)(c + 32)); // 첫 문자가 아닌데, 대문자 알파벳인 경우
-                } else {
-                    sb.append(c);
-                }
-            }
-            
-            if (c == ' ') isFirstCh = true;
-            else isFirstCh = false;
-        }
+        String[] sp = s.toLowerCase().split("");
+        boolean flag = true;
         
+        for(String ss : sp) {
+            sb.append(flag ? ss.toUpperCase() : ss);
+            flag = ss.equals(" ") ? true : false;
+        }
+
         return sb.toString();
-    }
+  }
 }
