efaa520e91da1149e85725e3f20b14f26ff47dc1
Minkyu Shin <98581610+uykm@users.noreply.github.com>
2024-06-30 23:28:48 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 30.41 ms, Memory: 89.3 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
@@ -1,30 +1,45 @@
 class Solution {
     public int[] solution(String s) {
         int[] answer = new int[2];
-        int zeroCnt = 0, transformCnt = 0;
-        
-        while (!s.equals("1")) {
-            int length = s.length();
-            for (char c : s.toCharArray()) {
-                if (c == '0') {
-                    length--;
-                    zeroCnt++;
-                }
-            }
-            
-            StringBuilder sb = new StringBuilder();
-            while (length > 1) {
-                sb.append((length % 2) + "");
-                length /= 2;
-            }
-            sb.append("1"); // MSB
-            
-            s = sb.reverse().toString();
-            transformCnt++;
+        int temp;
+        while( !s.equals("1") ) {
+            answer[1] += s.length();
+            s = s.replaceAll("0", "");
+            temp = s.length();
+            s = Integer.toBinaryString(temp);
+            answer[0]++;
+            answer[1] -= temp;
         }
-        
-        answer[0] = transformCnt; answer[1] = zeroCnt;
-        
-        return answer;
+        return answer;  
     }
 }
+// class Solution {
+//     public int[] solution(String s) {
+//         int[] answer = new int[2];
+//         int zeroCnt = 0, transformCnt = 0;
+        
+//         while (!s.equals("1")) {
+//             int length = s.length();
+//             for (char c : s.toCharArray()) {
+//                 if (c == '0') {
+//                     length--;
+//                     zeroCnt++;
+//                 }
+//             }
+            
+//             StringBuilder sb = new StringBuilder();
+//             while (length > 1) {
+//                 sb.append((length % 2) + "");
+//                 length /= 2;
+//             }
+//             sb.append("1"); // MSB
+            
+//             s = sb.reverse().toString();
+//             transformCnt++;
+//         }
+        
+//         answer[0] = transformCnt; answer[1] = zeroCnt;
+        
+//         return answer;
+//     }
+// }
