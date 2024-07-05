7cceb2242146c22dc9b82ec89687739557782246
Minkyu Shin <98581610+uykm@users.noreply.github.com>
2024-07-05 16:17:41 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.04 ms, Memory: 52.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        boolean flag = true;
+        int oneCnt = 0;
+        String s = Integer.toBinaryString(n);
+        for (char c : s.toCharArray()) {
+            if (c == '1') {
+                oneCnt++;
+            }
+        }
+        while (flag) {
+            String tmp = Integer.toBinaryString(++n);
+            int tmpCnt = 0;
+            for (char c : tmp.toCharArray()) {
+                if (c == '1') {
+                    tmpCnt++;
+                }
+            }
+            if (tmpCnt == oneCnt) {
+                flag = false;
+                answer = n;
+            }
+        }
+        return answer;
+    }
+}
