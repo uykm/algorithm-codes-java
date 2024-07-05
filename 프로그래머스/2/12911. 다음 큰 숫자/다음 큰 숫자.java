e7eeb2a2cebb9be9a8dbe5ffb918afa70d30ca3a
Minkyu Shin <98581610+uykm@users.noreply.github.com>
2024-07-05 16:29:02 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.02 ms, Memory: 52.4 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.java
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.java
@@ -1,27 +1,31 @@
 class Solution {
     public int solution(int n) {
-        int answer = 0;
-        boolean flag = true;
-        int oneCnt = 0;
-        String s = Integer.toBinaryString(n);
-        for (char c : s.toCharArray()) {
-            if (c == '1') {
-                oneCnt++;
-            }
-        }
-        while (flag) {
-            String tmp = Integer.toBinaryString(++n);
-            int tmpCnt = 0;
-            for (char c : tmp.toCharArray()) {
-                if (c == '1') {
-                    tmpCnt++;
-                }
-            }
-            if (tmpCnt == oneCnt) {
-                flag = false;
-                answer = n;
-            }
-        }
-        return answer;
+        // int answer = 0;
+        // boolean flag = true;
+        // int oneCnt = 0;
+        // String s = Integer.toBinaryString(n);
+        // for (char c : s.toCharArray()) {
+        //     if (c == '1') {
+        //         oneCnt++;
+        //     }
+        // }
+        // while (flag) {
+        //     String tmp = Integer.toBinaryString(++n);
+        //     int tmpCnt = 0;
+        //     for (char c : tmp.toCharArray()) {
+        //         if (c == '1') {
+        //             tmpCnt++;
+        //         }
+        //     }
+        //     if (tmpCnt == oneCnt) {
+        //         flag = false;
+        //         answer = n;
+        //     }
+        // }
+        // return answer;
+        
+        int a = Integer.bitCount(n);
+        while (Integer.bitCount(++n) != a) {}
+        return n;
     }
 }
