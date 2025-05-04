deea6e0742acbc75be4e7d57424a889a7de6f5ad
Minkyu Shin <98581610+uykm@users.noreply.github.com>
2025-05-04 18:09:32 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 100 ms, Memory: 14196 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
@@ -1,46 +1,43 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    
-    public static void main(String[] args) throws Exception {
-        
-        int min = 0; // 최솟값
-        
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String str = br.readLine();
-        
-        int num = 0;  // 문자열을 변환한 값
-        boolean isSub = false;
+
+        int min = 0;
+        boolean isMinus = false;
         StringBuilder sb = new StringBuilder();
-        
-        for (char c : str.toCharArray()) {
-            
+
+        for (char c : br.readLine().toCharArray()) {
             if ('0' <= c && c <= '9') {
                 sb.append(c);
                 continue;
             }
-            
-            num = Integer.parseInt(sb.toString());  // 연산자 사이에 있는 값
-            
-            if (isSub) min -= num;
-            else min += num;
-            
-            // c가 '-'인 경우 그 뒤에 나오는 값은 모두 뺴준다는 접근
-            if (c == '-') isSub = true;
-            
-            sb = new StringBuilder();  // sb 초기화
+
+            int num = Integer.parseInt(sb.toString());
+            sb = new StringBuilder();
+
+            if (isMinus) {
+                min -= num;
+            } else {
+                min += num;
+            }
+
+            if (c == '-') {
+                isMinus = true;
+            }
         }
-        
-        num = Integer.parseInt(sb.toString()); // 마지막 값
-        
-        // 마지막 값 처리
-        if (isSub) {
-             min -= num;
-        } else {
-            min += num;
+
+        if (sb.length() > 0) {
+            if (isMinus) {
+                min -= Integer.parseInt(sb.toString());
+            } else {
+                min += Integer.parseInt(sb.toString());
+            }
         }
-        
-        System.out.print(min);
+
+        System.out.println(min);
     }
 }
