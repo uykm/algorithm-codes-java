b6e9aade83bc891215b58af3025fbc2065119a21
Minkyu Shin <98581610+uykm@users.noreply.github.com>
2025-10-10 16:46:28 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 15.30 ms, Memory: 92 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/README.md
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/README.md
@@ -1,10 +1,10 @@
 # [level 1] 가장 많이 받은 선물 - 258712 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/258712#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/258712?language=java) 
 
 ### 성능 요약
 
-메모리: 83.2 MB, 시간: 6.82 ms
+메모리: 92 MB, 시간: 15.30 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 26일 02:26:41
+2025년 10월 10일 16:46:25
 
 ### 문제 설명
 
--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
@@ -2,53 +2,41 @@ import java.util.*;
 
 class Solution {
     public int solution(String[] friends, String[] gifts) {
-        int maxReceivedGifts = 0;
-        HashMap<String, HashMap<String, Integer>> sendMap = new HashMap<>();
-        HashMap<String, Integer> friendToPoint = new HashMap<>();
-        
-        for (String sender : friends) {
-            sendMap.putIfAbsent(sender, new HashMap<String, Integer>());
-            friendToPoint.put(sender, 0); // 모든 사람의 선물 지수를 0으로 세팅 -> 선물을 주지도 받지도 않은 사람을 따로 예외 처리해줄 필요 X
-            for (String receiver : friends) {
-                if (receiver.equals(sender)) continue;
-                sendMap.get(sender).put(receiver, 0); // 선물을 주고받은 기록이 없더라도 0으로 초기화
-            }
+        Map<String, Integer> friendToIndex = new HashMap<>();
+        for (int i = 0; i < friends.length; i++) {
+            friendToIndex.put(friends[i], i);
         }
+        
+        int[] points = new int[friends.length];
+        int[][] record = new int[friends.length][friends.length];
         
         for (String gift : gifts) {
-            String[] sr = gift.split(" ");
-            String sender = sr[0];
-            String receiver = sr[1];
+            String[] rs = gift.split(" ");
+            String sender = rs[0];
+            String receiver = rs[1];
             
-            HashMap<String, Integer> recToCnt = sendMap.get(sender);
-            recToCnt.put(receiver, recToCnt.get(receiver) + 1);
-            
-            friendToPoint.put(sender, friendToPoint.get(sender) + 1);
-            friendToPoint.put(receiver, friendToPoint.get(receiver) - 1);
+            points[friendToIndex.get(sender)]++;
+            points[friendToIndex.get(receiver)]--;
+            record[friendToIndex.get(sender)][friendToIndex.get(receiver)]++;
         }
+
+       int maxGifts = 0;
         
-        for (String sender : friends) {
-            int nextGifts = 0;
-            for (String receiver : friends) {
-                if (receiver.equals(sender)) continue;
-                
-                int sendCnt1 = sendMap.get(sender).get(receiver); // sender -> receiver 선물 개수
-                int sendCnt2 = sendMap.get(receiver).get(sender); // receiver -> sender 선물 개수
-                int point1 = friendToPoint.get(sender); // sender의 선물 지수
-                int point2 = friendToPoint.get(receiver); // receiver의 선물 지수
-                
-                if (sendCnt1 == sendCnt2) { // 주고받은 기록이 하나도 없거나 주고받은 수가 같은 경우
-                    if (point1 > point2) { // sender의 선물 지수가 더 높은 경우
-                        nextGifts++;
-                    }
-                } else if (sendCnt1 > sendCnt2) { // sender가 receiver에게 다음 달에 선물을 받아야 하는 경우
-                    nextGifts++;
-                }
-            }
-            
-            maxReceivedGifts = Math.max(maxReceivedGifts, nextGifts);
-        }
-        
-        return maxReceivedGifts;
+       for (int i = 0; i < friends.length; i++) {
+           int nextGifts = 0;
+           
+           for (int j = 0; j < friends.length; j++) {
+               if(i == j) continue; // 본인인 경우
+               
+               // sender가 receiver에게 다음 달에 선물을 받아야 하는 경우
+               if (record[i][j] > record[j][i]) {
+                   nextGifts++; 
+               } else if (record[i][j] == record[j][i] && points[i] > points[j]) {
+                   nextGifts++; 
+               }
+           }
+           maxGifts = Math.max(nextGifts, maxGifts);
+       }
+        return maxGifts;
     }
 }
