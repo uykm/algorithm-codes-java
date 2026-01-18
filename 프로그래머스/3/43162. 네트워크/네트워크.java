ab6991336392c1556b129895268a86a7984a09b4
Minkyu Shin <98581610+uykm@users.noreply.github.com>
2026-01-18 19:14:41 +0900

    [level 3] Title: 네트워크, Time: 0.28 ms, Memory: 90.4 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,48 +1,73 @@
 import java.util.*;
 
 class Solution {
-    
-    static List<List<Integer>> network = new ArrayList<>();
-    static boolean[] visited;
-    
+    boolean[] visited;
+
     public int solution(int n, int[][] computers) {
-        int networkCnt = 0;
-        
-        for (int i = 0; i < n; ++i) {
-            network.add(new ArrayList<>());
-        }
-        
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < n; ++j) {
-                if (i != j && computers[i][j] == 1) {
-                    addEdge(i, j);
-                }
-            }
-        }
-        
         visited = new boolean[n];
-        for (int i = 0; i < n; ++i) {
+        int cnt = 0;
+
+        for (int i = 0; i < n; i++) {
             if (!visited[i]) {
-                dfs(i);
-                networkCnt++;
+                dfs(i, computers, n);
+                cnt++;
             }
         }
-        
-        return networkCnt;
+        return cnt;
     }
-    
-    static void dfs(int node) {
-        for (int next : network.get(node)) {
-            if (!visited[next]) {
-                visited[next] = true;
-                dfs(next);
+
+    void dfs(int cur, int[][] computers, int n) {
+        visited[cur] = true;
+        for (int next = 0; next < n; next++) {
+            if (computers[cur][next] == 1 && !visited[next]) {
+                dfs(next, computers, n);
             }
         }
     }
-    
-    static void addEdge(int a, int b) {
-        network.get(a).add(b);
-        network.get(b).add(a);
-    }
-    
 }
+
+// class Solution {
+    
+//     static List<List<Integer>> network = new ArrayList<>();
+//     static boolean[] visited;
+    
+//     public int solution(int n, int[][] computers) {
+//         int networkCnt = 0;
+        
+//         for (int i = 0; i < n; ++i) {
+//             network.add(new ArrayList<>());
+//         }
+        
+//         for (int i = 0; i < n; ++i) {
+//             for (int j = i+1; j < n; ++j) {
+//                 if (i != j && computers[i][j] == 1) {
+//                     addEdge(i, j);
+//                 }
+//             }
+//         }
+        
+//         visited = new boolean[n];
+//         for (int i = 0; i < n; ++i) {
+//             if (!visited[i]) {
+//                 dfs(i);
+//                 networkCnt++;
+//             }
+//         }
+        
+//         return networkCnt;
+//     }
+    
+//     static void dfs(int node) {
+//         visited[node] = true;
+//         for (int next : network.get(node)) {
+//             if (!visited[next]) {
+//                 dfs(next);
+//             }
+//         }
+//     }
+    
+//     static void addEdge(int a, int b) {
+//         network.get(a).add(b);
+//         network.get(b).add(a);
+//     }
+// }
