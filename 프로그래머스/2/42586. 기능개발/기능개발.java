91bd8c0b12598cf716b674cb163529f510802891
Minkyu Shin <98581610+uykm@users.noreply.github.com>
2026-01-29 19:00:57 +0900

    [level 2] Title: 기능개발, Time: 3.22 ms, Memory: 95.8 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -4,7 +4,7 @@ class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
         List<Integer> answer = new ArrayList<>();
         
-        // 담겨있는 값: 가장 마지막으로 배포된 프로젝트까지 걸린 시간
+        // 담겨있는 값: 개발을 완료하고 배포 대기 중인 프로젝트들을 개발 완료하기 위해 필요했던 시간(일)
         Queue<Integer> passedDate = new LinkedList<>();
         
         for (int i = 0; i < speeds.length; ++i) {
@@ -14,6 +14,7 @@ class Solution {
             
             // 배포 시작: (아직 완료처리 되지 않은 프로젝트 ~ i-1 번째 프로젝트)
             if (!passedDate.isEmpty() && passedDate.peek() < requiredDate) {
+                // passedDate.peek(): 현재 배포 대기중인 프로젝트 중 개발 완료까지 가장 오래 걸린 시간
                 // passedDate.size(): 배포를 기다리고 있는 개발 완료된 프로젝트 수
                 answer.add(passedDate.size()); 
                 passedDate.clear();
