691d7d84e82e6b3507034bc2fba3014c307d6037
Minkyu Shin <98581610+uykm@users.noreply.github.com>
2026-01-29 18:55:23 +0900

    [level 2] Title: 기능개발, Time: 7.69 ms, Memory: 84.4 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -4,27 +4,25 @@ class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
         List<Integer> answer = new ArrayList<>();
         
-        Queue<Integer> queue = new LinkedList<>();
-        for (int i = 0; i < progresses.length; ++i) {
-            queue.add(i);
+        // 담겨있는 값: 가장 마지막으로 배포된 프로젝트까지 걸린 시간
+        Queue<Integer> passedDate = new LinkedList<>();
+        
+        for (int i = 0; i < speeds.length; ++i) {
+            double remain = (100 - progresses[i]) / (double) speeds[i];
+            // i 번째 프로젝트를 개발 완료하기 위해 필요한 시간(일)
+            int requiredDate = (int) Math.ceil(remain);
+            
+            // 배포 시작: (아직 완료처리 되지 않은 프로젝트 ~ i-1 번째 프로젝트)
+            if (!passedDate.isEmpty() && passedDate.peek() < requiredDate) {
+                // passedDate.size(): 배포를 기다리고 있는 개발 완료된 프로젝트 수
+                answer.add(passedDate.size()); 
+                passedDate.clear();
+            }
+            
+            passedDate.offer(requiredDate); // i번째 프로젝트까지 배포 완료됐다고 가정
         }
         
-        int start = 0;
-        while (queue.size() > 0) {
-            for (int i = start; i < progresses.length; ++i) {
-                progresses[i] += speeds[i];
-            }
-            
-            int cnt = 0;
-            while (queue.size() > 0 && progresses[queue.peek()] >= 100) {
-                queue.poll();
-                cnt++;
-            }
-            
-            
-            if (cnt > 0) answer.add(cnt);
-            start = cnt;
-        }
+        answer.add(passedDate.size());
         
         return answer.stream().mapToInt(i -> i).toArray();
     }
