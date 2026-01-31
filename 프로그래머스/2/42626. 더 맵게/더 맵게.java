fcbcef52c7e1ea1592ba9dc4064db792afffba7e
Minkyu Shin <98581610+uykm@users.noreply.github.com>
2026-02-01 02:18:00 +0900

    [level 2] Title: 더 맵게, Time: 1560.30 ms, Memory: 147 MB -BaekjoonHub

---
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] scoville, int K) {
+        int answer = 0;
+        
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        
+        for (int s : scoville) {
+            pq.offer(s);
+        }
+        
+        while (pq.size() >= 2 && pq.peek() < K) {
+            int best = pq.poll();
+            int second = pq.poll() * 2;
+            int newFood = best + second;
+            pq.offer(newFood);
+            answer++;
+        }
+        
+        if (pq.peek() < K) return -1;
+        
+        return answer;
+    }
+}
