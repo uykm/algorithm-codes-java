7564f2bde7260734664e1c1e4b071f7df2ff89e7
Minkyu Shin <98581610+uykm@users.noreply.github.com>
2026-02-12 21:43:33 +0900

    [level 2] Title: 모음 사전, Time: 10.79 ms, Memory: 80.6 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -28,7 +28,7 @@ class Solution {
         for (int i = 0; i < 5; ++i) {
             sb.append(words[i]);
             fillDict(sb, length + 1);
-            sb.deleteCharAt(sb.length() - 1); // 되돌리기
+            sb.deleteCharAt(sb.length() - 1); // "AAAAA" -> "AAAA" + "E"를 위한 되돌리기
         }
     }
 }
