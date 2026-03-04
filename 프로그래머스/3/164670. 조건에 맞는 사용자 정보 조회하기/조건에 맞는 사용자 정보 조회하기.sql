6e5ad191a28cedc65a319e48a3d64eac382c515d
Minkyu Shin <98581610+uykm@users.noreply.github.com>
2026-03-05 00:42:38 +0900

    [level 3] Title: 조건에 맞는 사용자 정보 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
@@ -0,0 +1,17 @@
+SELECT
+    u.USER_ID,
+    u.NICKNAME,
+    CONCAT(u.CITY, ' ', u.STREET_ADDRESS1, ' ', u.STREET_ADDRESS2) AS `전체주소`,
+    CONCAT(
+        SUBSTRING(u.TLNO, 1, 3), '-', 
+        SUBSTRING(u.TLNO, 4, 4), '-', 
+        SUBSTRING(u.TLNO, 8, 4)
+    ) AS `전화번호`
+FROM USED_GOODS_USER u
+JOIN (
+    SELECT WRITER_ID
+    FROM USED_GOODS_BOARD
+    GROUP BY WRITER_ID
+    HAVING COUNT(*) >= 3
+) b ON b.WRITER_ID = u.USER_ID
+ORDER BY u.USER_ID DESC
