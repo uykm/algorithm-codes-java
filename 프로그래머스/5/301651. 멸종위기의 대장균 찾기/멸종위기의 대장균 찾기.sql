9b0d53a8340e55ca5e2b1b0c0c37695d03a62ad6
Minkyu Shin <98581610+uykm@users.noreply.github.com>
2026-03-06 16:19:21 +0900

    [level 5] Title: 멸종위기의 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/5/301651. 멸종위기의 대장균 찾기/멸종위기의 대장균 찾기.sql
+++ 프로그래머스/5/301651. 멸종위기의 대장균 찾기/멸종위기의 대장균 찾기.sql
@@ -1,29 +1,30 @@
-WITH RECURSIVE T1 AS (
-    SELECT
-        ID,
-        PARENT_ID,
-        1 AS GENERATION
-    FROM ECOLI_DATA
-    WHERE PARENT_ID IS NULL
-    
-    UNION ALL
-    
-    SELECT
-        T2.ID,
-        T2.PARENT_ID,
-        GENERATION + 1 AS GENERATION
-    FROM T1
-    JOIN ECOLI_DATA AS T2
-        ON T1.ID = T2.PARENT_ID
-)
-SELECT 
-    COUNT(ID) AS COUNT,
-    GENERATION
-FROM T1
-WHERE ID NOT IN (
-    SELECT DISTINCT PARENT_ID
-    FROM T1
-    WHERE PARENT_ID IS NOT NULL
+WITH RECURSIVE 
+    CHILDS AS (
+        SELECT
+            ID,
+            PARENT_ID,
+            1 AS GENER
+        FROM ECOLI_DATA
+        WHERE PARENT_ID IS NULL
+
+        UNION ALL
+
+        SELECT
+            ed.ID,
+            ed.PARENT_ID,
+            GENER + 1 AS GENER
+        FROM CHILDS c
+        JOIN ECOLI_DATA ed
+            ON ed.PARENT_ID = c.ID
+    )
+SELECT
+    COUNT(p.ID) AS COUNT,
+    p.GENER AS GENERATION
+FROM CHILDS p
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM CHILDS c
+    WHERE p.ID = c.PARENT_ID
 )
 GROUP BY GENERATION
 ORDER BY GENERATION
