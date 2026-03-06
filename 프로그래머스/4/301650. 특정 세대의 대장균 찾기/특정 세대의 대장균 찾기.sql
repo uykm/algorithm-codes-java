d409984fb15b60d4ff08a14c6b7acd39fd5f8a75
Minkyu Shin <98581610+uykm@users.noreply.github.com>
2026-03-06 14:09:16 +0900

    [level 4] Title: 특정 세대의 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/301650. 특정 세대의 대장균 찾기/특정 세대의 대장균 찾기.sql
+++ 프로그래머스/4/301650. 특정 세대의 대장균 찾기/특정 세대의 대장균 찾기.sql
@@ -1,12 +1,21 @@
-SELECT
-    c.ID
-FROM
-    ECOLI_DATA c
-JOIN 
-    ECOLI_DATA p
-    ON c.PARENT_ID = p.ID
-JOIN
-    ECOLI_DATA g
-    ON p.PARENT_ID = g.ID
-WHERE
-    g.PARENT_ID IS NULL
+WITH RECURSIVE
+    GEN AS (
+        SELECT
+            ID,
+            1 AS GENER
+        FROM ECOLI_DATA
+        WHERE PARENT_ID IS NULL
+        
+        UNION ALL
+        
+        SELECT
+            ED.ID,
+            GEN.GENER + 1 AS GENER
+        FROM GEN
+        JOIN ECOLI_DATA ED ON GEN.ID = ED.PARENT_ID
+        WHERE GEN.GENER < 3
+    )
+SELECT ID
+FROM GEN
+WHERE GENER = 3
+ORDER BY ID;
