a35ebcd35ce6c2b4a24a6db8d339210e289587b5
Minkyu Shin <98581610+uykm@users.noreply.github.com>
2026-06-15 03:42:49 +0900

    Time: 6 ms (24.79%), Space: 43.5 MB (42.58%) - LeetHub

---
+++ LeetCode/Medium/0274-h-index/0274-h-index.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int hIndex(int[] citations) {
+
+        int answer = 0;
+        Arrays.sort(citations);
+
+        for (int i = 0; i < citations.length; ++i) {
+            if (citations.length - i <= citations[i]) {
+                answer = citations.length - i;
+                break;
+            }
+        }
+
+        return answer;
+
+    }
+}
