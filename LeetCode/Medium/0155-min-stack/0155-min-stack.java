098bc58b90fb85bf24aabd56da097ac72952330d
Minkyu Shin <98581610+uykm@users.noreply.github.com>
2026-07-01 05:23:50 +0900

    Time: 30 ms (48.79%), Space: 103 MB (6.7%) - LeetHub

---
+++ LeetCode/Medium/0155-min-stack/0155-min-stack.java
@@ -0,0 +1,35 @@
+class MinStack {
+
+    private Deque<Integer> stack;
+    private Deque<Integer> minStack;
+
+    public MinStack() {
+        stack = new ArrayDeque<>();
+        minStack = new ArrayDeque<>();
+    }
+
+    public void push(int value) {
+        stack.push(value);
+
+        if (minStack.isEmpty() || value <= minStack.peek()) {
+            minStack.push(value);
+        }
+    }
+
+    public void pop() {
+        int removed = stack.pop();
+
+        if (removed == minStack.peek()) {
+            minStack.pop();
+        }
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+
+}
